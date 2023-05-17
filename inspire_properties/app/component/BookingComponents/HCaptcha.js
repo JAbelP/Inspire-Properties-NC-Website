01edeb5456b501ba4175da4cad1a8fb4cb3d982d
Abel Pinales <abelpinales97@gmail.com>
2023-05-17 14:49:10 -0400

    Security Issue

--- inspire_properties/app/component/BookingComponents/HCaptcha.js
+++ inspire_properties/app/component/BookingComponents/HCaptcha.js
@@ -12,7 +12,7 @@ function HCaptchaComponent({ onVerify }) {
     <div className='pt-2'>
     {/* Render the hCaptcha component */}
     <HCaptcha
-     sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITEKEY} //	10000000-ffff-ffff-ffff-000000000001 //3401880a-95fe-4481-86fa-a258397649b5
+     sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITEKEY}
      onVerify={handleVerify}
       />
     </div>
