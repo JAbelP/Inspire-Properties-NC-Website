5e27924fbc0eec2f439014348bcb9704948295f3
Abel Pinales <abelpinales97@gmail.com>
2023-06-07 13:55:43 -0400

    updating Script

--- inspire_properties/app/Financing/page.tsx
+++ inspire_properties/app/Financing/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import MyHeader from '../component/Header'
 import MyFooter from '../component/Footer'
+import Script from 'next/script';
 
 function page() {
   return (
@@ -10,7 +11,7 @@ function page() {
             <div className='text-white bg-black h-60 sm:w-full w-80 sm:h-24 text-center text-6xl flex items-center justify-center'>
                 Need to finance a Project?
             </div>
-            <script src="https://widget.gethearth.com/script.js" id="hearth-script" data-orgid="39548" data-partner="inspire-properties-llc"></script>
+            <Script src="https://widget.gethearth.com/script.js" id="hearth-script" data-orgid="39548" data-partner="inspire-properties-llc"></script>
                 <iframe id="hearth-widget_calculator_v1"></iframe>
         </main>
         <footer className='inline-block'>
