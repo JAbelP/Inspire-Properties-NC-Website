0b57396c4bd0505a68d62b4525c9dc4771670d5f
Abel Pinales <abelpinales97@gmail.com>
2023-06-07 13:37:58 -0400

    Trial

--- inspire_properties/app/Financing/page.tsx
+++ inspire_properties/app/Financing/page.tsx
@@ -12,8 +12,6 @@ function page() {
             </div>
             <script src="https://widget.gethearth.com/script.js" id="hearth-script" data-orgid="39548" data-partner="inspire-properties-llc"></script>
                 <iframe id="hearth-widget_calculator_v1"></iframe>
-
-ok
         </main>
         <footer className='inline-block'>
         <MyFooter/>
