8b5c949140591209a1dffa9017943b05cb5f2f33
Abel Pinales <abelpinales97@gmail.com>
2023-06-07 11:39:06 -0400

    Finance Page skeleton Done

---
+++ inspire_properties/app/Financing/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import MyHeader from '../component/Header'
+import MyFooter from '../component/Footer'
+
+function page() {
+  return (
+     <div className='bg-white min-h-screen flex flex-col'>
+        <MyHeader/>
+        <main className='flex-grow'>
+        
+        </main>
+        <footer className='inline-block'>
+        <MyFooter/>
+      </footer>
+     </div>
+    
+  )
+}
+
+export default page
