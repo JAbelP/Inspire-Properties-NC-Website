a87c37e8a6c41422944c29838e241260e119da38
Abel Pinales <abelpinales97@gmail.com>
2023-06-12 13:36:24 -0400

    Trial

--- inspire_properties/app/Financing/page.tsx
+++ inspire_properties/app/Financing/page.tsx
@@ -11,7 +11,7 @@ function page() {
             <div className='text-white bg-black h-60 sm:w-full w-80 sm:h-24 text-center text-6xl flex items-center justify-center'>
                 Need to finance a Project?
             </div>
-            <script src="https://widget.gethearth.com/script.js" id="hearth-script" data-orgid="39548" data-partner="inspire-properties-llc" />
+            <Script src="https://widget.gethearth.com/script.js" id="hearth-script" data-orgid="39548" data-partner="inspire-properties-llc" />
               <iframe id="hearth-widget_calculator_v1"></iframe>
         </main>
         <footer className='inline-block'>
