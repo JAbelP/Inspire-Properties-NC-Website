a0d7333d156495eadf8f3fb57217945fafafe17d
JAbelP <59974707+JAbelP@users.noreply.github.com>
2023-08-15 20:00:34 -0400

    What google wants (#37)
    * Finished Privacy policy changed grey in submit
    
    * Add before and afters fix up the main page.
    
    * the postraphy

--- inspire_properties/app/page.tsx
+++ inspire_properties/app/page.tsx
@@ -33,6 +33,60 @@ export default function Home() {
         <div className="w-full lg:h-[40rem]  bg-gray-300  overflow-hidden">
           <Slider slides={slides} />
         </div>
+       <div>
+        
+       </div>
+
+       <div className="bg-gray-100 py-10">
+            <div className="container mx-auto px-4">
+              <h2 className="text-3xl font-semibold text-gray-800 mb-4">
+                Power Washing & Deck Staining Services
+              </h2>
+              <p className="text-gray-600 mb-8">
+                We offer top-notch power washing and deck staining services to
+                keep your property looking fresh and well-maintained. Our expert
+                team ensures thorough cleaning and professional staining for
+                stunning results.
+              </p>
+              <button className="bg-blue-500 text-white px-6 py-2 rounded-lg">
+                Learn More
+              </button>
+            </div>
+          </div>
+          <div className="bg-gray-200 py-10">
+            <div className="container mx-auto px-4">
+              <h2 className="text-3xl font-semibold text-gray-800 mb-4">
+                Lawn Care Services
+              </h2>
+              <p className="text-gray-600 mb-8">
+                Transform your outdoor space with our comprehensive lawn care
+                services. From mowing and fertilizing to landscaping and
+                maintenance, we ensure your lawn stays lush and inviting.
+              </p>
+              <button className="bg-green-500 text-white px-6 py-2 rounded-lg">
+                Learn More
+              </button>
+            </div>
+          </div>
+          <div className="bg-gray-300 py-10">
+            <div className="container mx-auto px-4">
+              <h2 className="text-3xl font-semibold text-gray-800 mb-4">
+                Home Improvement Solutions
+              </h2>
+              <p className="text-gray-600 mb-8">
+                Enhance your property&apos;s value and comfort with our range of home
+                improvement services. From renovations and repairs to
+                installations, we bring your vision to life with quality
+                workmanship.
+              </p>
+              <button className="bg-red-500 text-white px-6 py-2 rounded-lg">
+                Learn More
+              </button>
+            </div>
+          </div>
+
+
+
         <MainGeneralContactUsComponent/>
       </div>
       </main>
