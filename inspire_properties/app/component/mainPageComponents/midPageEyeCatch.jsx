7383418834f5f12bab4c714187e090734cf8856e
Abel Pinales <abelpinales97@gmail.com>
2023-09-10 11:58:07 -0400

    Final upload

--- inspire_properties/app/component/mainPageComponents/midPageEyeCatch.jsx
+++ inspire_properties/app/component/mainPageComponents/midPageEyeCatch.jsx
@@ -12,7 +12,7 @@ function MidPageEyeCatch() {
           <h2 className="text-3xl font-semibold text-gray-800 mb-4">
             Power Washing & Deck Staining Services
           </h2>
-          <p className="text-gray-600 mb-8">
+          <p className="text-gray-600 mb-8 lg:text-xl lg:font-semibold">
             We offer top-notch power washing and deck staining services to keep
             your property looking fresh and well-maintained. Our expert team
             ensures thorough cleaning and professional staining for stunning
@@ -26,18 +26,18 @@ function MidPageEyeCatch() {
             alt="Deck that was powerwashed and stained"
           />
           <Link href="/Book">
-            <button className="bg-blue-500 text-white px-6 py-2 rounded-lg border-2 border-black font-semibold hover:text-black hover:bg-blue-300">
+            <button className="bg-blue-500 text-white text-3xl px-12 py-2 rounded-lg border-2 border-black font-semibold hover:text-black hover:bg-blue-300">
               Book with Us Today
             </button>
           </Link>
         </div>
       </div>
-      <div className="bg-gray-200 py-10">
+      <div className="bg-gray-200 py-10 ">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-semibold text-gray-800 mb-4">
             Lawn Care Services
           </h2>
-          <p className="text-gray-600 mb-8">
+          <p className="text-gray-600 mb-8 lg:text-xl lg:font-semibold">
             Transform your outdoor space with our comprehensive lawn care
             services. From mowing and fertilizing to landscaping and
             maintenance, we ensure your lawn stays lush and inviting.
@@ -45,12 +45,12 @@ function MidPageEyeCatch() {
           <Image
             className="rounded-xl mb-2"
             src="/HomePageImages/Lawn.jpg"
-            width={400}
-            height={400}
+            width={500}
+            height={500}
             alt="Deck that was powerwashed and stained"
           />
           <Link href="/Book">
-            <button className="bg-green-500 text-white px-6 py-2 rounded-lg border-2 border-black font-semibold hover:text-black hover:bg-green-300">
+            <button className="bg-green-500 text-white text-3xl px-12 py-2 rounded-lg border-2 border-black font-semibold hover:text-black hover:bg-green-300">
               Book with Us Today
             </button>
           </Link>
@@ -61,30 +61,30 @@ function MidPageEyeCatch() {
           <h2 className="text-3xl font-semibold text-gray-800 mb-4">
             Home Improvement Solutions
           </h2>
-          <p className="text-gray-600 mb-8">
+          <p className="text-gray-600 mb-8 lg:text-xl lg:font-semibold">
             Enhance your property&apos;s value and comfort with our range of
             home improvement services. From renovations and repairs to
             installations, we bring your vision to life with quality
             workmanship.
           </p>
           <div>
-            <ImgComparisonSlider width={2} height={2} className="rounded-lg">
+            <ImgComparisonSlider width={4} height={4} className="rounded-lg">
               <img
                 slot="first"
                 src="/HomePageImages/Image2Before.jpg"
-                height={220}
-                width={220}
+                height={320}
+                width={420}
               />
               <img
                 slot="second"
                 src="/HomePageImages/Image2After.jpg"
-                height={220}
-                width={220}
+                height={320}
+                width={420}
               />
             </ImgComparisonSlider>
           </div>
           <Link href="/Book">
-            <button className="bg-red-500 text-white px-6 py-2 rounded-lg capitalize border-2 border-black font-semibold hover:text-black hover:bg-red-300">
+            <button className="bg-red-500 text-white  text-3xl px-12 py-2 rounded-lg capitalize border-2 border-black font-semibold hover:text-black hover:bg-red-300">
               Book with Us Today
             </button>
           </Link>
